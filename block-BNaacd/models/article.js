3d852d58ecb2334383408f91e3fcd837c984920b
priyabadatiya <priyabadatiya@gmail.com>
2021-07-04 02:41:47 +0530

    code

--- block-BNaacd/models/article.js
+++ block-BNaacd/models/article.js
@@ -4,11 +4,6 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     name : String,
-    email:String,
-    age : Number
-})
-
-var userSchema= new Schema({
     email:{type: String, lowercase:true},
     age:{type:Number, default:0}
 })
