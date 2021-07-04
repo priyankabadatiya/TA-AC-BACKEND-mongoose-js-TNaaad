f0a8af8b73de23b3d891b849b7921eb26b1fe844
priyabadatiya <priyabadatiya@gmail.com>
2021-07-04 22:01:23 +0530

    Explain with code

---
+++ block-BNaacn/app.js
@@ -0,0 +1,18 @@
+
+var express = require("express")
+
+var logger =require("morgan")
+
+var mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/sample",(err)=>{
+    console.log(err?err:"connected to database")
+})
+
+var app = express();
+
+app.use(logger('dev'))
+
+app.listen(4000,()=>{
+    console.log("server is listening at port 4k")
+})
